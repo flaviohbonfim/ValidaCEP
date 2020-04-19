ba57e4bd4c5ae2550fa3609279f6c7c59f733712
Flavio Henrique Bonfim <flaviohbonfim@live.com>
2020-04-19 18:49:05 -0300

    Add project files.

---
+++ frmValidaCEP.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace ValidaCEP
+{
+    partial class frmValidaCEP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.txtLogradouro = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtComplemento = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLocalidade = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtIBGE = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtGia = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite o CEP:";
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(88, 12);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(146, 20);
+            this.txtCEP.TabIndex = 1;
+            // 
+            // txtLogradouro
+            // 
+            this.txtLogradouro.Location = new System.Drawing.Point(88, 38);
+            this.txtLogradouro.Name = "txtLogradouro";
+            this.txtLogradouro.Size = new System.Drawing.Size(366, 20);
+            this.txtLogradouro.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Logradouro:";
+            // 
+            // txtComplemento
+            // 
+            this.txtComplemento.Location = new System.Drawing.Point(88, 64);
+            this.txtComplemento.Name = "txtComplemento";
+            this.txtComplemento.Size = new System.Drawing.Size(366, 20);
+            this.txtComplemento.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Complemento:";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(88, 90);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(366, 20);
+            this.txtBairro.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Bairro:";
+            // 
+            // txtLocalidade
+            // 
+            this.txtLocalidade.Location = new System.Drawing.Point(88, 116);
+            this.txtLocalidade.Name = "txtLocalidade";
+            this.txtLocalidade.Size = new System.Drawing.Size(366, 20);
+            this.txtLocalidade.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Localidade:";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(88, 142);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(366, 20);
+            this.txtEstado.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Estado:";
+            // 
+            // txtIBGE
+            // 
+            this.txtIBGE.Location = new System.Drawing.Point(88, 168);
+            this.txtIBGE.Name = "txtIBGE";
+            this.txtIBGE.Size = new System.Drawing.Size(366, 20);
+            this.txtIBGE.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 172);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "IBGE:";
+            // 
+            // txtGia
+            // 
+            this.txtGia.Location = new System.Drawing.Point(88, 194);
+            this.txtGia.Name = "txtGia";
+            this.txtGia.Size = new System.Drawing.Size(366, 20);
+            this.txtGia.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 198);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "GIA:";
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(240, 9);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 16;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmValidaCEP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 228);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.txtGia);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtIBGE);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtLocalidade);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtComplemento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLogradouro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.label1);
+            this.Name = "frmValidaCEP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Valida CEP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.TextBox txtLogradouro;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtComplemento;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLocalidade;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtIBGE;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtGia;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnConsultar;
+    }
+}
+
---
+++ frmValidaCEP.cs
@@ -0,0 +1,40 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidaCEP
+{
+    public partial class frmValidaCEP : Form
+    {
+        public frmValidaCEP()
+        {
+            InitializeComponent();
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                var address = await cepClient.GetAddressAsync(txtCEP.Text);
+                txtLogradouro.Text = address.Logradouro;
+                txtComplemento.Text = address.Complemento;
+                txtBairro.Text = address.Bairro;
+                txtLocalidade.Text = address.Localidade;
+                txtEstado.Text = address.UF;
+                txtIBGE.Text = address.Ibge;
+                txtGia.Text = address.Gia;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao consultar CEP: " + ex.Message, "Erro", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
